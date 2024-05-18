21ca8e0191098d99d480822b969934af1d9b5193
xu-minghao317 <85087583+xu-minghao317@users.noreply.github.com>
2024-05-18 18:15:03 +0900

    feat: add XDG_STATE_HOME environment variable

--- zshenv.zsh
+++ zshenv.zsh
@@ -15,3 +15,4 @@ export LANG=en_US.UTF-8
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
