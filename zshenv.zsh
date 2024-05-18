d938fff785641b931c95db4cc66e648a2edba07b
xu-minghao317 <85087583+xu-minghao317@users.noreply.github.com>
2024-05-18 18:02:04 +0900

    fix: remove LC_ALL setting to avoid overriding other locale settings

--- zshenv.zsh
+++ zshenv.zsh
@@ -8,7 +8,8 @@ typeset -U path PATH fpath FPATH manpath MANPATH INFOPATH
 
 # Set LOCALE
 export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
+# Generally, setting LANG is sufficient. If you need to enforce it for all locale categories, uncomment the following line:
+# export LC_ALL=en_US.UTF-8
 
 # XDG Base Directory Specification
 export XDG_CONFIG_HOME="$HOME/.config"
