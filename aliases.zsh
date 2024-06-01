99740fbb595be467d33f4038d7bd33a1764b4425
xu-minghao317 <85087583+xu-minghao317@users.noreply.github.com>
2024-06-02 02:43:30 +0900

    fix: delete all '.DS_Store' in node lib (#9)

--- aliases.zsh
+++ aliases.zsh
@@ -63,5 +63,5 @@ alias rebuild_completion='rm -v ~/.zcompdump* && exec zsh'
 # Remove `.DS_Store` files so `npm` can finally chill and install global packages on macOS
 # Currently, npm guys think it's our fault, so let's clean up their mess
 if [[ "$OS_TYPE" == "Darwin" ]]; then
-    alias fix_npm="fd -H -d 1 '^\.DS_Store$' \$(npm list -g | head -1) -tf -X rm -i"
+    alias fix_npm="fd -H '^\.DS_Store$' $(npm list -g | head -1) -tf -X rm -f"
 fi
